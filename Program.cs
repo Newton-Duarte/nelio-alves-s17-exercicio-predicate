30f2660eacd7191c215d7935f52143f5efc05899
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-08-30 19:34:21 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿List<Product> products = new()
+{
+  new Product("Tv", 900),
+  new Product("Mouse", 50),
+  new Product("Tablet", 350.50),
+  new Product("HD Case", 80.90)
+};
+
+static bool ProductsWithPriceGreaterThanOrEqualTo100(Product product)
+{
+  return product.Price >= 100;
+}
+
+products.RemoveAll(ProductsWithPriceGreaterThanOrEqualTo100);
+
+foreach(var product in products)
+{
+  System.Console.WriteLine($"{product.Name} - {product.Price:F2}");
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# C# Curso Nélio Alves - Seção 17, Exercício Predicate
